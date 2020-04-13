3a69b87eb7ee57085c5c477c78568d1794dbebe4
Maxime Tarel <maxime.tarel@epitech.eu>
2020-04-13 12:45:15 +0200

    ADD itinial android studio project build

---
+++ app/app/src/main/java/com/epitech/yep_project1_2019_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.epitech.yep_project1_2019_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
