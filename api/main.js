5fadad155784e4217bc4774167936050158dc9af
Guillaume Astorgis <guillaume.astorgis@epitech.eu>
2020-04-16 18:37:09 +0200

    Added basic websocket server

--- api/main.js
+++ api/main.js
@@ -1,1 +1,30 @@
-console.log("Started")
+const ws = require('ws');
+
+const port = process.env.PORT || 8081
+
+const server = new ws.Server({ port: port })
+
+// const connections = {
+// 	pending: {}
+// }
+
+server.on('connection', (websocket) => {
+
+	websocket.on('message', (message) => {
+		console.log('received: %s', message)
+	})
+
+	websocket.on('error', (error) => {
+		console.error('error: %s', error)
+	})
+
+	websocket.on('close', () => {
+		console.log('closed websocket')
+	})
+
+	websocket.on('ping', () => {
+		websocket.pong('pong', false)
+	})
+
+	websocket.send('zbebzbeb')
+})
