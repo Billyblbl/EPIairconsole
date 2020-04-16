2d3bea871cfdc66d9ffcc0d60f01833a8ef457a7
Guillaume Astorgis <guillaume.astorgis@epitech.eu>
2020-04-16 20:04:39 +0200

    Transmitters
    - Added transmitter registration
    - Added transmition

--- api/main.js
+++ api/main.js
@@ -4,14 +4,31 @@ const port = process.env.PORT || 8081
 
 const server = new ws.Server({ port: port })
 
-// const connections = {
-// 	pending: {}
-// }
+const connections = {
+	pending: []
+}
 
-server.on('connection', (websocket) => {
+server.on('connection', (websocket, req) => {
+	connections.pending.push(websocket)
+
 
 	websocket.on('message', (message) => {
-		console.log('received: %s', message)
+		console.log('received: ', message)
+		try {
+			switch (message.type) {
+				case 'register': register(websocket, message.code, message.role)
+					break;
+				case 'transmit': transmit(websocket, message.data)
+					break;
+				default:
+					break;
+			}
+		} catch (error) {
+			console.error(error.message)
+			if (message.msgid) {
+				websocket.send({type: 'error', msgid: message.msgid, reason: error.message})
+			}
+		}
 	})
 
 	websocket.on('error', (error) => {
@@ -26,5 +43,53 @@ server.on('connection', (websocket) => {
 		websocket.pong('pong', false)
 	})
 
-	websocket.send('zbebzbeb')
+	console.log(`Added connection ${req.connection.remoteAddress}`)
+
+	websocket.send({type: 'ready'})
 })
+
+console.log(`Websocket API server listening on port ${port}`)
+
+function register(websocket, code, role) {
+	if (code === undefined) {
+		if (role === 'emitter') { throw new Error(`Cannot register as emitter without a connection code`) }
+		code = genCode()
+		websocket.send({type: 'code', code: code})
+	}
+	if (connections[code] === undefined || connections[code] === null) {
+		connections[code] = {
+			emitter: null,
+			receiver: null
+		}
+	}
+	connections[code][role] = websocket
+	websocket.room = connections[code]
+	websocket.role = role
+	connections.pending.splice(connections.pending.indexOf(websocket))
+
+	//TODO better code gen because this one is one hell of a potential perf sink
+	function genCode() {
+		let nums
+		do {
+			nums = [genNum(), genNum(), genNum(), genNum(), genNum(), genNum()].join('')
+		} while (connections[nums])
+		return nums
+
+		function genNum() { return Math.floor(Math.random() * 9) }
+	}
+}
+
+function transmit(websocket, data) {
+	switch (websocket.role) {
+		case 'emitter':
+			websocket.room.receiver.send(data)
+			break;
+		case 'receiver':
+			websocket.room.emitter.send(data)
+			break;
+		default:
+			websocket.room.receiver.send(data)
+			websocket.room.emitter.send(data)
+			break;
+	}
+}
